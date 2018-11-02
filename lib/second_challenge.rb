e78f1cc08ca26b84cd588ae74d3432bfea96aceb
Corina Talavera <corina.talavera@gmail.com>
2018-11-02 23:31:44 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,22 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague"},
+      :matriarch => {name: "Lady Montague"},
+      :hero => {name: "Romeo"},
+      :hero_friends => [
+         {name: "Benvolio",}
+         {name: "Mercutio",}
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet"},
+      :matriarch => {name: "Lady Capulet"},
+      :heroine => {name: "Juliet"},
+      :heroine_friends => [
+          {name: "Steven"},
+          {name: "Nurse"}
+      ]
+   }
+}
 end
